4c9c1909d6f0e68b9dc60407040d72b785bddcb4
kalyankumar <kalyank608@gmail.com>
2025-07-18 12:37:57 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -9,12 +9,12 @@ DOMAIN_NAME="daws85s.fun"
 for INSTANCE in ${INSTANCES[@]}
 do 
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0598484c641e7e708 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$INSTANCE}]" --query "Instances[0].InstanceId" --output text)
-  if [ $instance !="frontend" ]
+  if [ $INSTANCE !="frontend" ]
   then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
   fi
-  echo "$instance IP address: $IP"    
+  echo "$INSTANCE IP address: $IP"    
          
 done
