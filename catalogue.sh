c9615ec583ecc6d8abaa93c34b92ff06045c0ede
kalyankumar <kalyank608@gmail.com>
2025-07-19 17:20:12 +0530

    colors

--- catalogue.sh
+++ catalogue.sh
@@ -77,7 +77,13 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB client"
 
-mongosh --host mongodb.daws85s.fun </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loading Data into MongoDB"
+STATUS=$(mongosh --host mongodb.daws85s.fun --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws85s.fun </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading Data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi        
 
 
